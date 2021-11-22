46a503339a655d41cf0d2ef13ed3be2530540414
Aral Roca Gomez <aral-rg@hotmail.com>
2021-11-23 00:38:50 +0100

    Add panel to add a modification

--- panel/src/AddModification/index.jsx
+++ panel/src/AddModification/index.jsx
@@ -1,5 +1,42 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import AceEditor from 'react-ace';
+
+import 'ace-builds/src-noconflict/mode-json';
+import 'ace-builds/src-noconflict/theme-github';
+import 'ace-builds/src-noconflict/theme-monokai';
+
+import { useStore } from '../store';
 
 export default function AddModification() {
-  return <div>TODO</div>;
+  const epoch = useRef(Date.now());
+  const [showAdd, setShowAdd] = useStore.showAdd();
+  const [selectedStore] = useStore.selectedStore();
+  const [history] = useStore.stores[selectedStore].history();
+  const [newHistory, setNewHistory] = useState(history[0].store);
+  const theme = window.matchMedia('(prefers-color-scheme: dark)').matches
+    ? 'monokai'
+    : 'github';
+
+  return (
+    <>
+      <AceEditor
+        mode="json"
+        value={newHistory}
+        theme={theme}
+        style={{ width: '100%', height: 'calc(100vh - 60px)' }}
+        onChange={setNewHistory}
+        name={epoch}
+        editorProps={{ $blockScrolling: true }}
+      />
+      <footer className="save-modification">
+        <button
+          onClick={() => setShowAdd(false)}
+          className="transparent-button"
+        >
+          Cancel
+        </button>
+        <button className="transparent-button">Apply store modification</button>
+      </footer>
+    </>
+  );
 }
